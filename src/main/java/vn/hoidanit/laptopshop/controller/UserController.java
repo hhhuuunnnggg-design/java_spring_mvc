a35d0528e06ddca5bbf45608f8f1b3be5ef9b0ff
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2024-10-28 09:43:31 +0700

    b35

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,32 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+// @Controller
+// public class UserController {
+
+//     @RequestMapping("/")
+//     public String getHomePage() {
+//         return "hello home page";
+//     }
+// }
+
+@RestController
+public class UserController {
+
+    // @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
